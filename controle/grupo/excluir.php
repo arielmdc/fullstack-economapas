85dee6795bc7040ea8ebc3f3bb97ea42132279e3
Ariel Mota <arielmcarvalho@gmail.com>
2021-08-25 07:09:33 -0300

    fix: alteração na mensagem de exclusão de grupo

--- controle/grupo/excluir.php
+++ controle/grupo/excluir.php
@@ -6,4 +6,17 @@ require '../../modelo/Grupo.php';
 var_dump($_POST);
 
 
+$grupo = new Grupo();
+
+$idGrupo = ($_POST['id']); 
+
+$result = $grupo->deleta($idGrupo);
+
+if($result['tipo']){
+    $_SESSION['mensagem2']['content'] = $result['mensagem'];
+}else{
+    $_SESSION['mensagem2']['content'] = $result['mensagem'];
+}
+
+header('Location: /visao/dashboard.php');
 //header('Location: /visao/dashboard.php');
