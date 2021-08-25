044cb9f99632b1df0a4cd678a917dd515f06db54
Ariel Mota <arielmcarvalho@gmail.com>
2021-08-25 07:10:19 -0300

    fix: validação de grupo vazio

--- controle/grupo/criar_grupo.php
+++ controle/grupo/criar_grupo.php
@@ -7,14 +7,20 @@ $grupo = new Grupo();
 
 $id_usuario = (isset($_SESSION['id_usuario'])) ? $_SESSION['id_usuario'] : null ;
 $grupo_nome = (isset($_POST['grupo_nome'])) ? $_POST['grupo_nome'] : null ;
-
-$result = $grupo->cadastro($grupo_nome, $id_usuario);
-
-if($result['tipo']){
-    $_SESSION['idGrupo_nome'] = $result['idGrupo_nome'];
-    header('Location: /visao/dashboard.php');
-    $_SESSION['mensagem2']['content'] = $result['mensagem2'];
+if(!empty($grupo_nome)){
+    $result = $grupo->cadastro($grupo_nome, $id_usuario);
+    if($result['tipo']){
+        $_SESSION['idGrupo_nome'] = $result['idGrupo_nome'];
+        header('Location: /visao/dashboard.php');
+        $_SESSION['mensagem2']['content'] = $result['mensagem2'];
+    }else{
+        $_SESSION['mensagem']['content'] = $result['mensagem'];
+        header('Location: /visao/dashboard.php');
+    }
 }else{
-    $_SESSION['mensagem']['content'] = $result['mensagem'];
+    $_SESSION['mensagem']['content'] = 'Nome do grupo não pode ser vazio';
     header('Location: /visao/dashboard.php');
 }
+
+
+
