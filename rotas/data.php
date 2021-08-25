f153f346f5055a2f7ebcd97db1da76815f69a2ba
Ariel Mota <arielmcarvalho@gmail.com>
2021-08-25 07:07:43 -0300

    feat: add case cidades

--- rotas/data.php
+++ rotas/data.php
@@ -7,6 +7,20 @@ if(!isset($_GET['req'])){
 $req = $_GET['req'];
 
 switch ($req) {
+    case 'cidades':
+        require_once '../modelo/Cidade.php';
+        $cidade = new Cidade();
+        $id_grupo = $_POST['id_grupo'];
+        $resp = $cidade->retornaCidades($id_grupo);
+        echo json_encode($resp);
+        break;
+    case 'getGrupos':
+        require_once '../modelo/Grupo.php';
+        session_start();
+        $grupo = new Grupo();
+        $result = $grupo->getGrupoByID($id_grupo);
+        echo json_encode($result);
+        break;
 
     case 'getGruposByUsuario':
         require_once '../modelo/Grupo.php';
