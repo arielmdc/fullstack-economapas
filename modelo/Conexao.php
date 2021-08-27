eff96a9d6ebb638c685c987c32d4f3637fadeb9d
Ariel Mota <arielmcarvalho@gmail.com>
2021-08-26 21:12:49 -0300

    fix: fix bd name

--- modelo/Conexao.php
+++ modelo/Conexao.php
@@ -5,7 +5,7 @@ class Conexao{
     public static function Connect(){
 
         $opcoes = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-        $connect = new PDO("mysql:host=localhost;dbname=economapas2", 'root', '123456', $opcoes); 
+        $connect = new PDO("mysql:host=localhost;dbname=economapas", 'root', '123456', $opcoes); 
         return $connect;
     }
 }
